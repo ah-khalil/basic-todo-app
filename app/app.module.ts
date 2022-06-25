e9dd94cb45b4b9419142eddf156c088abff3b1ba
ah-khalil <ali.haytham.khal@gmail.com>
2022-06-25 22:12:51 +0800

    removed additional 'app' from selector

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderBannerComponent } from './components/header-banner/header-banner.component';
 import { TodoMenuComponent } from './components/todo-menu/todo-menu.component';
 import { TodoMenuItemComponent } from './components/todo-menu-item/todo-menu-item.component';
-import { TodoListContainerComponent } from './todo-list-container/todo-list-container.component';
+import { TodoListContainerComponent } from './components/todo-list-container/todo-list-container.component';
 
 @NgModule({
   declarations: [
